410300f1459975f5a4db2b3473585d234390e67f
Veyrathal <meggiecarr0403@gmail.com>
2026-02-17 10:42:17 -0600

    Fix API_BASE for production

--- static/config.js
+++ static/config.js
@@ -1,5 +1,9 @@
 // static/js/config.js
-export const API_BASE   = "http://127.0.0.1:8008";
+export const API_BASE   = 
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+  ? "http://127.0.0.1:8008"
+  : "";
 export const PROJECT_ID = 1;          // the project you created in Swagger
 export const CHAT_ID    = 1;          // your chat id (integer)
 export const X_USER_ID  = "veyra-001"; // same header you used in Swagger
